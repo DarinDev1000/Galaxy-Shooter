12892f3e6afd5163b48ef5838d91f094ee85c9ff
Darin Crawmer <darindev1000@gmail.com>
2022-11-19 20:00:12 -0800

    move player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.Translate(5 * Time.deltaTime * Vector3.forward);
     }
 }
