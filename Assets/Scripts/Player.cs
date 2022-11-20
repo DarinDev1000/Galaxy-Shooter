85de053e14980d913d0dad1b47df4fd05b0f4c3d
Darin Crawmer <darindev1000@gmail.com>
2022-11-19 20:08:58 -0800

    player speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Parts of a variable
+    // 0 attribute
+    // 1 public or private reference
+    // 2 data type (int, float, bool, and strings)
+    // 3 variable name. Standard is to underscore private variables
+    // (4) optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(5 * Time.deltaTime * Vector3.forward);
+        transform.Translate(_speed * Time.deltaTime * Vector3.right);
     }
 }
